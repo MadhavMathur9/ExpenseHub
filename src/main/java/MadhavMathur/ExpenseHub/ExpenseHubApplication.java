18d50efed33d65c6ad88ca45727e5ab9b444ff42
Madhav Mathur <madhav.23bcon1381@jecrcu.edu.in>
2026-03-28 11:20:04 +0530

    spring project initialize

---
+++ src/main/java/MadhavMathur/ExpenseHub/ExpenseHubApplication.java
@@ -0,0 +1,13 @@
+package MadhavMathur.ExpenseHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MadhavMathur/ExpenseHub/ExpenseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package MadhavMathur.ExpenseHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
