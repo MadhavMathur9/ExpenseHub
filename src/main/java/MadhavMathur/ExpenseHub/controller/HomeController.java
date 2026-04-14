dac1b7c6bf4be5f2db5bb7c1399a0a8163ec36d2
Madhav Mathur <madhav.23bcon1381@jecrcu.edu.in>
2026-04-14 16:25:37 +0530

    Debug Java code

--- src/main/java/MadhavMathur/ExpenseHub/controller/HomeController.java
+++ src/main/java/MadhavMathur/ExpenseHub/controller/HomeController.java
@@ -1,7 +1,12 @@
 package MadhavMathur.ExpenseHub.controller;
 
+
+@RestController
+@RequestMapping({"/staus", "/health"})
+
 public class HomeController 
 {
+    @GetMapping
     public String healthCheck()  
     {
         return "Application is running fine";
