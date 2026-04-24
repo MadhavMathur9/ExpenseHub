c0e82312212a78ab63f1fa80875d194c7f31413e
Madhav Mathur <madhav.23bcon1381@jecrcu.edu.in>
2026-04-24 15:18:58 +0530

    Working on Profile

--- src/main/java/MadhavMathur/ExpenseHub/controller/ProfileController.java
+++ src/main/java/MadhavMathur/ExpenseHub/controller/ProfileController.java
@@ -1,5 +1,26 @@
 package MadhavMathur.ExpenseHub.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import MadhavMathur.ExpenseHub.dto.ProfileDTO;
+import MadhavMathur.ExpenseHub.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class ProfileController {
 
+    private final ProfileService profileService;
+//Handler Method
+
+     @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+
+    }
 }
